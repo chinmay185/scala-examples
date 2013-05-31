cce236ab4b920c91156c3a05acab1d085661cd37
Chinmay Naik <cnaik@equalexperts.com>
2013-05-31 15:40:34 +0530

    generified the filterChain method.

--- MathWorks.scala
+++ MathWorks.scala
@@ -20,7 +20,7 @@ object MathWorks extends App {
 		n % 5 == 0
 	}
 
-	def filterChain(numbers: Seq[Int], fs: ((Int) => (Boolean))*): Seq[Int] = {
+	def filterChain[T](numbers: Seq[T], fs: ((T) => (Boolean))*): Seq[T] = {
 		fs match {
 			case list if list.isEmpty => numbers
 			case l => filterChain(numbers.filter(fs.head), fs.tail:_*)
@@ -29,13 +29,13 @@ object MathWorks extends App {
 
 	override def main(args: Array[String]) = {
 		val numbers = Range(1, 50)
-		println("Even numbers : " + filterChain(numbers, isEven))
-		println("Odd numbers : " + filterChain(numbers, isOdd))
-		println("Prime numbers : " + filterChain(numbers, isPrime))
-		println("Odd Prime numbers : " + filterChain(numbers, isOdd, isPrime))
-		println("Even Prime numbers : " + filterChain(numbers, isEven, isPrime))
-		println("Multiples of 3 : " + filterChain(numbers, multiplesOf3))
-		println("Multiples of 5 : " + filterChain(numbers, multiplesOf5))
-		println("Multiples of 3 and 5 : " + filterChain(numbers, multiplesOf3, multiplesOf5))
+		println("Even numbers : " + filterChain[Int](numbers, isEven))
+		println("Odd numbers : " + filterChain[Int](numbers, isOdd))
+		println("Prime numbers : " + filterChain[Int](numbers, isPrime))
+		println("Odd Prime numbers : " + filterChain[Int](numbers, isOdd, isPrime))
+		println("Even Prime numbers : " + filterChain[Int](numbers, isEven, isPrime))
+		println("Multiples of 3 : " + filterChain[Int](numbers, multiplesOf3))
+		println("Multiples of 5 : " + filterChain[Int](numbers, multiplesOf5))
+		println("Multiples of 3 and 5 : " + filterChain[Int](numbers, multiplesOf3, multiplesOf5))
 	}
 }
